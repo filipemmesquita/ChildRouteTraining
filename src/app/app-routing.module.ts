61fcd455f411b56f16f0e76665dfd565dc8eebfb
Filipe Mesquita <filipemmesquita95@gmail.com>
2024-09-01 17:45:57 -0300

    Add first child route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstPageComponent } from './components/first-page/first-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'first-page',
+    loadChildren: ()=> import('./components/first-page/first-page.module').then(m=>m.FirstPageModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
